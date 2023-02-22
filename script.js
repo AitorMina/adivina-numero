33566f7522014d2c3a30b445401d5193401bd59f
aitor_mina <aitormina@hotmail.com>
2023-02-22 13:13:01 +0100

    Listener en checkbutton ok

--- script.js
+++ script.js
@@ -9,18 +9,28 @@ const numberField = document.querySelector('.number')
 
 //2do paso:crear las variables que necesitamos
 
-const score = 20
+let score = 20
 const highscore = 0
 
 const MIN_NUMBER = 1
 const MAX_NUMBER = 20
 const secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + MIN_NUMBER
-const number = guessField.value
 
-//3er paso: crear las funciones que necesitamos
-
-scoreField.textContent = 10
+//3er paso: añadir un listener al checkButton , agregarle un evento y mostrar
+//un mensaje que diga si el número es mayor o menor en el campo
+//messageField
 
 checkButton.addEventListener('click', function () {
-  alert('Hola mundo')
+  const number = guessField.value
+  if (number < secretNumber) {
+    messageField.textContent = 'El número es menor'
+    score--
+    scoreField.textContent = score
+  } else if (number > secretNumber) {
+    messageField.textContent = 'El número es mayor'
+    score--
+    scoreField.textContent = score
+  } else {
+    messageField.textContent = '¡Acertaste!'
+  }
 })
