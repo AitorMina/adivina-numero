d86f27aba2c94608fde7f6d0fa6a1e562c631e33
aitor_mina <aitormina@hotmail.com>
2023-02-22 12:38:03 +0100

    Crear las variables

--- script.js
+++ script.js
@@ -7,15 +7,17 @@ const messageField = document.querySelector('.message')
 const highscoreField = document.querySelector('.highscore')
 const numberField = document.querySelector('.number')
 
-console.log(
-  scoreField,
-  checkButton,
-  againButton,
-  guessField,
-  highscoreField,
-  messageField,
-  numberField
-)
+//2do paso:crear las variables que necesitamos
+
+const score = 20
+const highscore = 0
+
+const MIN_NUMBER = 1
+const MAX_NUMBER = 20
+const secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + MIN_NUMBER
+const number = guessField.value
+
+//3er paso: crear las funciones que necesitamos
 
 scoreField.textContent = 10
 
